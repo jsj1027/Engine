3c11a90576bcfa57dbe4011592246ad32f26d602
Javis Johnson <jsj1027@g.rit.edu>
2019-06-02 14:27:45 -0400

    init lost lal my old funny commit messages hella sad

---
+++ src/fp.cpp
@@ -0,0 +1,39 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+} 
+
+int main(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    glViewport(0, 0, 800, 600);
+    
+    void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    glfwTerminate();
+    return 0;
+}
